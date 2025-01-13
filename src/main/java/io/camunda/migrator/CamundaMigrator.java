ec2c474af8cd1cadb9f1516ab8e52f4311da0563
Petros Savvidis <petros.savvidis@camunda.com>
2025-01-13 15:42:16 +0200

    WIP Initial C7 engine

--- src/main/java/io/camunda/migrator/CamundaMigrator.java
+++ src/main/java/io/camunda/migrator/CamundaMigrator.java
@@ -2,23 +2,44 @@ package io.camunda.migrator;
 
 import io.camunda.db.rdbms.sql.ProcessInstanceMapper;
 import io.camunda.db.rdbms.write.domain.ProcessInstanceDbModel;
+import jakarta.annotation.PostConstruct;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class CamundaMigrator {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(CamundaMigrator.class);
 
-  @Autowired
-  public ProcessInstanceMapper processInstanceMapper;
+  private final ProcessEngine processEngine;
+  private final ProcessInstanceMapper processInstanceMapper;
 
-  @Bean
-  public String migrate() {
+  public CamundaMigrator(
+      ProcessEngine processEngine,
+      ProcessInstanceMapper processInstanceMapper
+  ) {
+    this.processEngine = processEngine;
+    this.processInstanceMapper = processInstanceMapper;
+  }
+
+  @PostConstruct
+  public void migrate() {
     LOGGER.info("Migrating process instances");
+
+    HistoryService historyService = processEngine.getHistoryService();
+    List<HistoricProcessInstance> allHistoricProcessInstances = historyService.createHistoricProcessInstanceQuery()
+        .list();
+
+    LOGGER.info("Retrieved {} historic process instances from C7 Process Engine", allHistoricProcessInstances.size());
+
     processInstanceMapper.insert(new ProcessInstanceDbModel(
         1L,
         "123",
@@ -33,8 +54,6 @@ public class CamundaMigrator {
         "elementId",
         1 // version
     ));
-
-    return "";
   }
 
 }
---
+++ src/main/java/io/camunda/migrator/config/C7ProcessEngineConfiguration.java
@@ -0,0 +1,25 @@
+package io.camunda.migrator.config;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class C7ProcessEngineConfiguration {
+
+  @Bean
+  public ProcessEngine processEngine(@Value("${migrator.camunda7.engine.jdbc-url}") String jdbcUrl,
+                                      @Value("${migrator.camunda7.engine.jdbc-username}") String jdbcUsername,
+                                      @Value("${migrator.camunda7.engine.jdbc-password}") String jdbcPassword,
+                                      @Value("${migrator.camunda7.engine.jdbc-driver}") String jdbcDriver) {
+    return ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
+        .setJdbcUrl(jdbcUrl)
+        .setJdbcUsername(jdbcUsername)
+        .setJdbcPassword(jdbcPassword)
+        .setJdbcDriver(jdbcDriver)
+        .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+        .buildProcessEngine();
+  }
+}
