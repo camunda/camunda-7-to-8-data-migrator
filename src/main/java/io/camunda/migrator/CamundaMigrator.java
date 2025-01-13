838f5184d5186309483d78f4bee9c30d734ff82d
Tassilo Weidner <3015690+tasso94@users.noreply.github.com>
2025-01-13 09:30:06 +0100

    wip

---
+++ src/main/java/io/camunda/migrator/CamundaMigrator.java
@@ -0,0 +1,55 @@
+package io.camunda.migrator;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.engine.HistoryService;
+import io.camunda.db.rdbms.RdbmsService;
+
+import java.util.List;
+
+public class CamundaMigrator {
+
+  public static void main(String[] args) {
+    // Configure and build the C7 process engine
+    ProcessEngine c7Engine = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration()
+        .setJdbcUrl("jdbc:h2:tcp://localhost/~/camunda7")
+        .setJdbcUsername("sa")
+        .setJdbcPassword("")
+        .setJdbcDriver("org.h2.Driver")
+        .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+        .buildProcessEngine();
+
+    // Configure and build the C8 process engine
+    RdbmsService c8Service = new RdbmsService();
+
+    // Deploy process definition to C7
+
+    // Produce history data on C7
+    c7Engine.getRuntimeService().startProcessInstanceById("Process_07e8as6");
+
+    // Deploy process definition to C8
+    // Todo how to transform the C7 process to C8.
+
+    // Migrate history data from C7 to C8
+    migrateHistoricProcessInstances(c7Engine, c8Service);
+
+    // Close C7 engine
+    c7Engine.close();
+  }
+
+  private static void migrateHistoricProcessInstances(ProcessEngine c7Engine, RdbmsService c8Service) {
+    HistoryService c7HistoryService = c7Engine.getHistoryService();
+    List<HistoricProcessInstance> instances = c7HistoryService.createHistoricProcessInstanceQuery().list();
+
+    for (HistoricProcessInstance instance : instances) {
+      // Migrate the process instance to C8
+      migrateProcessInstance(instance);
+    }
+  }
+
+  private static void migrateProcessInstance(HistoricProcessInstance instance) {
+    // C8 RDBMS ProcessInstanceWriter to persist the data to the database
+  }
+
+}
--- src/main/java/io/camunda/migrator/Main.java
+++ src/main/java/io/camunda/migrator/Main.java
@@ -1,3 +1,5 @@
+package io.camunda.migrator;
+
 public class Main {
 
   public static void main(String[] args) {
