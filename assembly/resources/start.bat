7453283bd711e520f3f8e9da3d28cccc3d8c4447
gergely.juhasz@camunda.com <gergely.juhasz@camunda.com>
2025-05-08 10:35:39 +0200

    choe(assembly): update start.bat

--- assembly/resources/start.bat
+++ assembly/resources/start.bat
@@ -4,6 +4,10 @@ setlocal
 REM Get the directory of the script
 set "BASEDIR=%~dp0"
 set "CONFIGURATION=%BASEDIR%configuration\application.yml"
+set "CLASSPATH=%BASEDIR%userlib"
+
+set "JAR_PATH=.\lib\c7-data-migrator.jar"
+set "COMMON_OPTS=-Dloader.path=%CLASSPATH% -Dspring.config.location=file:%CONFIGURATION%"
 
 set OPTIONS_HELP=Options:
 set OPTIONS_HELP=%OPTIONS_HELP%^^^
@@ -14,25 +18,26 @@ REM Check number of arguments
 if not "%2"=="" (
   echo Error: Only one flag allowed.
   echo Usage: %~nx0 [--runtime|--history]
+  echo %OPTIONS_HELP%
   exit /b 1
 )
 
 if not "%1"=="" (
   if "%1"=="--runtime" (
     echo Starting migration with flag: %1
-    java -jar .\lib\c7-data-migrator-distro-0.0.1-SNAPSHOT.jar %1 --spring.config.location=file:"%CONFIGURATION%"
+    java %COMMON_OPTS% -jar "%JAR_PATH%" %1
   ) else if "%1"=="--history" (
     echo Starting migration with flag: %1
-    java -jar .\lib\c7-data-migrator-distro-0.0.1-SNAPSHOT.jar %1 --spring.config.location=file:"%CONFIGURATION%"
+    java %COMMON_OPTS% -jar "%JAR_PATH%" %1
   ) else (
     echo Invalid flag: %1
-    echo Usage: run.bat [--runtime|--history]
+    echo Usage: %~nx0 [--runtime|--history]
     echo %OPTIONS_HELP%
     exit /b 1
   )
 ) else (
   echo Starting application without migration flag.
-  java -jar .\lib\c7-data-migrator-distro-0.0.1-SNAPSHOT.jar --spring.config.location=file:"%CONFIGURATION%"
+  java %COMMON_OPTS% -jar "%JAR_PATH%"
 )
 
 endlocal
