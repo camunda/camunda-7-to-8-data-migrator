6fea1c92825a96f27c9b8b6a4f27194c39bc7d0a
Yana Vasileva <yana.vasileva@camunda.com>
2025-05-15 10:48:24 +0200

    adopt rdbms snapshot changes

--- core/src/main/java/io/camunda/migrator/config/RdbmsConfiguration.java
+++ core/src/main/java/io/camunda/migrator/config/RdbmsConfiguration.java
@@ -179,7 +179,8 @@ public class RdbmsConfiguration {
         variableMapper,
         null,
         batchOperationReader,
-        jobMapper);
+        jobMapper,
+        null);
   }
 
   @Bean
@@ -220,6 +221,7 @@ public class RdbmsConfiguration {
         userTaskReader,
         formReader,
         mappingReader,
-        batchOperationReader);
+        batchOperationReader,
+        null);
   }
 }
