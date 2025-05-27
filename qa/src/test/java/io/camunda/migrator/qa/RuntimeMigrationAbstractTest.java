f4e69b535c42d04fe47765c635c6a3116eb3e724
Helene <21290204+HeleneW-dot@users.noreply.github.com>
2025-05-27 11:06:50 +0200

    feat(IT): add cleanup mechanism for tests (#38)

--- qa/src/test/java/io/camunda/migrator/qa/RuntimeMigrationAbstractTest.java
+++ qa/src/test/java/io/camunda/migrator/qa/RuntimeMigrationAbstractTest.java
@@ -9,10 +9,19 @@ package io.camunda.migrator.qa;
 
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.DeploymentEvent;
+import io.camunda.client.api.search.response.ProcessInstance;
 import io.camunda.migrator.RuntimeMigrator;
+import io.camunda.migrator.history.IdKeyMapper;
 import io.camunda.process.test.api.CamundaSpringProcessTest;
+import jakarta.annotation.PostConstruct;
+
+import java.util.List;
+
 import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.repository.Deployment;
+import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,14 +29,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 @CamundaSpringProcessTest
 public abstract class RuntimeMigrationAbstractTest {
 
+  // Migrator ---------------------------------------
+
   @Autowired
   protected RuntimeMigrator runtimeMigrator;
+
+  @Autowired
+  private IdKeyMapper idKeyMapper;
+
+  // C7 ---------------------------------------
 
   @Autowired
   protected RepositoryService repositoryService;
 
   @Autowired
+  protected RuntimeService runtimeService;
+
+  @Autowired
+  protected TaskService taskService;
+
+  // C8 ---------------------------------------
+
+  @Autowired
   protected CamundaClient camundaClient;
+
+  @AfterEach
+  public void cleanup() {
+    // C7
+    repositoryService.createDeploymentQuery().list().forEach(d -> repositoryService.deleteDeployment(d.getId(), true));
+
+    // C8
+    List<ProcessInstance> items = camundaClient.newProcessInstanceSearchRequest().send().join().items();
+    items.forEach(i -> camundaClient.newDeleteResourceCommand(i.getProcessInstanceKey()));
+
+    // Migrator table
+    idKeyMapper.findAllProcessInstanceIds().forEach(id -> idKeyMapper.delete(id));
+  }
 
   protected void deployCamunda7Process(String resourcePath) {
     Deployment deployment = repositoryService.createDeployment().addClasspathResource(resourcePath).deploy();
@@ -37,7 +74,9 @@ public abstract class RuntimeMigrationAbstractTest {
   }
 
   protected void deployCamunda8Process(String resourcePath) {
-    DeploymentEvent deployment = camundaClient.newDeployResourceCommand().addResourceFromClasspath(resourcePath).send().join();
+    DeploymentEvent deployment = camundaClient.newDeployResourceCommand().addResourceFromClasspath(resourcePath).send()
+        .join();
+
     if (deployment == null) {
       throw new IllegalStateException("Could not deploy process");
     }
