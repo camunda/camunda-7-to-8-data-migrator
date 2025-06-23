94a1295adb8fa165ee45cb520889f5997b673753
Yana Vasileva <yana.vasileva@camunda.com>
2025-06-23 17:45:42 +0200

    adjust xml spin test

--- qa/src/test/java/io/camunda/migrator/qa/SpinVariablesTest.java
+++ qa/src/test/java/io/camunda/migrator/qa/SpinVariablesTest.java
@@ -8,20 +8,25 @@
 package io.camunda.migrator.qa;
 
 import static io.camunda.process.test.api.assertions.ProcessInstanceSelectors.byProcessId;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.camunda.client.api.command.ClientException;
+import io.camunda.client.api.search.response.Variable;
 import io.camunda.process.test.api.CamundaAssert;
-import org.camunda.bpm.engine.variable.value.TypedValue;
+import java.util.List;
+import org.awaitility.Awaitility;
+import org.camunda.spin.json.SpinJsonNode;
 import org.camunda.spin.plugin.variable.SpinValues;
 import org.camunda.spin.plugin.variable.value.JsonValue;
 import org.camunda.spin.plugin.variable.value.XmlValue;
+import org.camunda.spin.xml.SpinXmlElement;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class SpinVariablesTest extends RuntimeMigrationAbstractTest {
 
   @Test
-  @Disabled
   public void shouldSetSpinJsonVariable() throws JsonProcessingException {
     // deploy processes
     deployProcessInC7AndC8("simpleProcess.bpmn");
@@ -35,20 +40,21 @@ public class SpinVariablesTest extends RuntimeMigrationAbstractTest {
         + "\"post code\" : 1234"
         + "}"
         + "}";
-        JsonValue jsonValue = SpinValues.jsonValue(json).create();
+    JsonValue jsonValue = SpinValues.jsonValue(json).create();
 
     runtimeService.setVariable(simpleProcessInstance.getId(), "var", jsonValue);
-    TypedValue c7var = runtimeService.getVariableTyped(simpleProcessInstance.getId(), "var", false);
+    SpinJsonNode c7value = (SpinJsonNode) runtimeService.getVariable(simpleProcessInstance.getId(), "var");
 
     // when running runtime migration
     runtimeMigrator.start();
 
     // then
     CamundaAssert.assertThat(byProcessId("simpleProcess"))
-        .hasVariable("var", c7var.getValue().toString());
+        .hasVariable("var", c7value.toString());
   }
 
   @Test
+  @Disabled // TODO https://github.com/camunda/camunda-bpm-platform/issues/5246
   public void shouldSetXmlVariable() throws JsonProcessingException {
     // deploy processes
     deployProcessInC7AndC8("simpleProcess.bpmn");
@@ -56,20 +62,35 @@ public class SpinVariablesTest extends RuntimeMigrationAbstractTest {
     // given process state in c7
     var simpleProcessInstance = runtimeService.startProcessInstanceByKey("simpleProcess");
 
-    String xml = "<customer xmlns=\"http:\\/\\/camunda.org/example\" name=\"Jonny\">"
+    String street = "<street>12 High Street</street>";
+    String postcode = "<postCode>1234</postCode>";
+    String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+        + "<customer xmlns=\"http:\\/\\/camunda.org/example\" name=\"Jonny\">"
         + "<address>"
-        + "<street>12 High Street</street>"
-        + "<postCode>1234</postCode>"
+        + street
+        + postcode
         + "</address>"
         + "</customer>";
     XmlValue xmlValue = SpinValues.xmlValue(xml).create();
-    runtimeService.setVariable(simpleProcessInstance.getId(), "var", xml);
+    runtimeService.setVariable(simpleProcessInstance.getId(), "var", xmlValue);
+    SpinXmlElement c7value = (SpinXmlElement) runtimeService.getVariable(simpleProcessInstance.getId(), "var");
+
 
     // when running runtime migration
     runtimeMigrator.start();
 
     // then
-    CamundaAssert.assertThat(byProcessId("simpleProcess"))
-        .hasVariable("var", xml);
+    //    CamundaAssert.assertThat(byProcessId("simpleProcess"))
+    //        .hasVariable("var", xml);
+
+    Awaitility.await().ignoreException(ClientException.class).untilAsserted(() -> {
+
+      List<Variable> items = camundaClient.newVariableSearchRequest().filter(f -> f.name("var")).send().join().items();
+
+      assertThat(items).hasSize(1);
+      assertThat(items.get(0).getValue()).contains(street);
+      assertThat(items.get(0).getValue()).contains(postcode);
+
+    });
   }
 }
