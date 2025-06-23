2c7036830d890eed9e74f06c0060b22b465fc033
Gergely Juhasz <25517896+venetrius@users.noreply.github.com>
2025-06-23 13:24:48 +0200

    chore(qa): add eventSubprocess tests (#73)
    related to: camunda/camunda-bpm-platform/issues/5167

---
+++ qa/src/test/java/io/camunda/migrator/qa/element/EventSubprocessMigrationTests.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+package io.camunda.migrator.qa.element;
+
+import static io.camunda.migrator.qa.MigrationTestConstants.LEGACY_ID_VAR_NAME;
+import static io.camunda.process.test.api.CamundaAssert.assertThat;
+import static io.camunda.process.test.api.assertions.ElementSelectors.byId;
+import static io.camunda.process.test.api.assertions.ProcessInstanceSelectors.byProcessId;
+import static io.camunda.process.test.api.assertions.UserTaskSelectors.byTaskName;
+
+import org.camunda.bpm.engine.ManagementService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventSubprocessMigrationTests extends AbstractElementMigrationTest {
+  @Autowired
+  private RuntimeService runtimeService;
+
+  @Autowired
+  private ManagementService managementService;
+
+  @Test
+  public void shouldMigrateNonInterruptingEventSubprocess() {
+    // given
+    deployProcessInC7AndC8("eventSubprocess.bpmn");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("eventSubprocessId");
+    // Send signal event, to trigger the eventSubprocess in C7
+    runtimeService.signalEventReceived("SignalEventName");
+
+    // when
+    runtimeMigrator.start();
+
+    // then both user tasks exist in C8
+    assertThat(byProcessId("eventSubprocessId")).isActive()
+        .hasActiveElements(byId("userTaskId"))
+        .hasActiveElements(byId("subprocessUserTaskId"))
+        .hasVariable(LEGACY_ID_VAR_NAME, processInstance.getProcessInstanceId());
+    assertThat(byTaskName("userTaskName")).isCreated().hasElementId("userTaskId");
+    assertThat(byTaskName("subprocessUserTaskName")).isCreated().hasElementId("subprocessUserTaskId");
+  }
+
+  @Test
+  public void migrateInterruptingEventSubprocess() {
+    // given
+    deployProcessInC7AndC8("interruptingEventSubprocess.bpmn");
+    ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("eventSubprocessId");
+    // Send signal event, to trigger the eventSubprocess in C7
+    runtimeService.signalEventReceived("SignalEventName");
+
+    // when
+    runtimeMigrator.start();
+
+    // then canceled user task does not exist in C8
+    assertThat(byProcessId("eventSubprocessId")).isActive()
+        .hasNoActiveElements(byId("userTaskId"))
+        .hasActiveElements(byId("subprocessUserTaskId"))
+        .hasVariable(LEGACY_ID_VAR_NAME, processInstance.getProcessInstanceId());
+    assertThat(byTaskName("subprocessUserTaskName")).isCreated().hasElementId("subprocessUserTaskId");
+  }
+}
