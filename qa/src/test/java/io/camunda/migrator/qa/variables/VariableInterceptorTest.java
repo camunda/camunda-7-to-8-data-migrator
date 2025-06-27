480cf5ea15c0a1750c72eb22be98d7c7a013da0f
Yana Vasileva <yana.vasileva@camunda.com>
2025-06-27 10:19:32 +0200

    improve test

--- qa/src/test/java/io/camunda/migrator/qa/variables/VariableInterceptorTest.java
+++ qa/src/test/java/io/camunda/migrator/qa/variables/VariableInterceptorTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.migrator.qa.RuntimeMigrationAbstractTest;
 import io.camunda.process.test.api.CamundaAssert;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,20 +20,28 @@ public class VariableInterceptorTest extends RuntimeMigrationAbstractTest {
   TestVariableInterceptor interceptor;
 
   @Test
-  public void shouldInvokeInterceptor(CapturedOutput output) {
+  public void shouldInvokeTestInterceptor(CapturedOutput output) {
     // deploy processes
     deployProcessInC7AndC8("simpleProcess.bpmn");
+    deployProcessInC7AndC8("userTaskProcess.bpmn");
 
-    // given process state in c7
+    // given processes state in c7
     var simpleProcessInstance = runtimeService.startProcessInstanceByKey("simpleProcess");
+    runtimeService.setVariable(simpleProcessInstance.getId(), "var", "value");
+    simpleProcessInstance = runtimeService.startProcessInstanceByKey("userTaskProcessId");
     runtimeService.setVariable(simpleProcessInstance.getId(), "var", "value");
 
     // when running runtime migration
     runtimeMigrator.start();
 
+    // then two instances and two interceptor invocations
     CamundaAssert.assertThat(byProcessId("simpleProcess"))
         .hasVariable("var", "value");
-    assertThat(output.getOut()).contains("Hello from interceptor");
+    CamundaAssert.assertThat(byProcessId("userTaskProcessId"))
+        .hasVariable("var", "value");
 
+    assertThat(output.getOut()).contains("Hello from interceptor");
+    Matcher matcher = Pattern.compile("Hello from interceptor").matcher(output.getOut());
+    assertThat(matcher.results().count()).isEqualTo(2);
   }
 }
