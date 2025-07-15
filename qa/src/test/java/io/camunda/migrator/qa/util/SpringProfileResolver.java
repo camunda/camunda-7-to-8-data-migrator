ce53dc91331037f0fc8903b436ac34b41252c667
Joaquín <joaquin.felici@camunda.com>
2025-07-15 18:31:55 +0200

    fix(test): Make WithSpringProfile extendable and other fixes
    Related to https://github.com/camunda/camunda-bpm-platform/issues/5262

--- qa/src/test/java/io/camunda/migrator/qa/util/SpringProfileResolver.java
+++ qa/src/test/java/io/camunda/migrator/qa/util/SpringProfileResolver.java
@@ -8,8 +8,11 @@
 package io.camunda.migrator.qa.util;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.jetbrains.annotations.NotNull;
-import org.junit.platform.commons.util.AnnotationUtils;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.test.context.ActiveProfilesResolver;
 
 /**
@@ -19,10 +22,15 @@ public final class SpringProfileResolver implements ActiveProfilesResolver {
 
   @Override
   public String @NotNull [] resolve(@NotNull Class<?> testClass) {
-    WithSpringProfile annotation = AnnotationUtils.findAnnotation(testClass, WithSpringProfile.class).orElse(null);
-    String activeProfiles = System.getProperty("spring.profiles.active", "");
-    String profile = annotation == null ? "" : annotation.value();
-    activeProfiles = activeProfiles + "," + profile;
-    return Arrays.stream(activeProfiles.split("\\s*,\\s*")).toArray(String[]::new);
+    var activeProfiles = getActiveProfiles();
+    var annotatedProfiles = AnnotatedElementUtils.findAllMergedAnnotations(testClass, WithSpringProfile.class).stream().map(WithSpringProfile::value).collect(Collectors.toSet());
+    return Stream.concat(activeProfiles.stream(), annotatedProfiles.stream()).distinct().toArray(String[]::new);
+  }
+
+  public static @NotNull List<String> getActiveProfiles() {
+    return Arrays.stream(System.getProperty("spring.profiles.active", "")
+        .split("\\s*,\\s*"))
+        .filter(s -> !s.isBlank())
+        .toList();
   }
 }
