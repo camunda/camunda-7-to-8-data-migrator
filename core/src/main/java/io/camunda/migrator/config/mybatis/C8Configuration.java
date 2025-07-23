f1b9925332513cf6b695ef4a4075a1283ded0bbf
tasso94 <3015690+tasso94@users.noreply.github.com>
2025-07-23 13:23:24 +0200

    chore(core): adapt latest RDBMS changes from snapshot (#144)

--- core/src/main/java/io/camunda/migrator/config/mybatis/C8Configuration.java
+++ core/src/main/java/io/camunda/migrator/config/mybatis/C8Configuration.java
@@ -26,6 +26,7 @@ import io.camunda.db.rdbms.read.service.ProcessInstanceDbReader;
 import io.camunda.db.rdbms.read.service.RoleDbReader;
 import io.camunda.db.rdbms.read.service.SequenceFlowDbReader;
 import io.camunda.db.rdbms.read.service.TenantDbReader;
+import io.camunda.db.rdbms.read.service.UsageMetricTUDbReader;
 import io.camunda.db.rdbms.read.service.UsageMetricsDbReader;
 import io.camunda.db.rdbms.read.service.UserDbReader;
 import io.camunda.db.rdbms.read.service.UserTaskDbReader;
@@ -49,6 +50,7 @@ import io.camunda.db.rdbms.sql.RoleMapper;
 import io.camunda.db.rdbms.sql.SequenceFlowMapper;
 import io.camunda.db.rdbms.sql.TenantMapper;
 import io.camunda.db.rdbms.sql.UsageMetricMapper;
+import io.camunda.db.rdbms.sql.UsageMetricTUMapper;
 import io.camunda.db.rdbms.sql.UserMapper;
 import io.camunda.db.rdbms.sql.UserTaskMapper;
 import io.camunda.db.rdbms.sql.VariableMapper;
@@ -110,215 +112,220 @@ public class C8Configuration extends AbstractConfiguration {
   }
 
   @Bean
-  public MapperFactoryBean<BatchOperationMapper> batchOperationMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<BatchOperationMapper> batchOperationMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, BatchOperationMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<AuthorizationMapper> authorizationMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<AuthorizationMapper> authorizationMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, AuthorizationMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<DecisionDefinitionMapper> decisionDefinitionMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<DecisionDefinitionMapper> decisionDefinitionMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, DecisionDefinitionMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<DecisionInstanceMapper> decisionInstanceMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<DecisionInstanceMapper> decisionInstanceMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, DecisionInstanceMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<DecisionRequirementsMapper> decisionRequirementsMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<DecisionRequirementsMapper> decisionRequirementsMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, DecisionRequirementsMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<FlowNodeInstanceMapper> flowNodeInstanceMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<FlowNodeInstanceMapper> flowNodeInstanceMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, FlowNodeInstanceMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<GroupMapper> groupInstanceMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<GroupMapper> groupInstanceMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, GroupMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<IncidentMapper> incidentMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<IncidentMapper> incidentMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, IncidentMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<ProcessInstanceMapper> processInstanceMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<ProcessInstanceMapper> processInstanceMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, ProcessInstanceMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<ProcessDefinitionMapper> processDeploymentMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<ProcessDefinitionMapper> processDeploymentMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, ProcessDefinitionMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<TenantMapper> tenantMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<TenantMapper> tenantMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, TenantMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<VariableMapper> variableMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<VariableMapper> variableMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, VariableMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<RoleMapper> roleMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<RoleMapper> roleMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, RoleMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<UserMapper> userMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<UserMapper> userMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, UserMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<UserTaskMapper> userTaskMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<UserTaskMapper> userTaskMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, UserTaskMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<FormMapper> formMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<FormMapper> formMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, FormMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<MappingRuleMapper> mappingMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<MappingRuleMapper> mappingMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, MappingRuleMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<ExporterPositionMapper> exporterPosition(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<ExporterPositionMapper> exporterPosition(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, ExporterPositionMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<PurgeMapper> purgeMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<PurgeMapper> purgeMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, PurgeMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<SequenceFlowMapper> sequenceFlowMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<SequenceFlowMapper> sequenceFlowMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, SequenceFlowMapper.class);
   }
 
   @Bean
-  public MapperFactoryBean<MetricsRecorder> metricsRecorder(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<MetricsRecorder> metricsRecorder(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, MetricsRecorder.class);
   }
 
   @Bean
-  public MapperFactoryBean<UsageMetricMapper> usageMetricMapper(final @Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+  public MapperFactoryBean<UsageMetricMapper> usageMetricMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
     return createMapperFactoryBean(c8SqlSessionFactory, UsageMetricMapper.class);
   }
 
   @Bean
-  public VariableDbReader variableRdbmsReader(final VariableMapper variableMapper) {
+  public MapperFactoryBean<UsageMetricTUMapper> usageMetricTUMapper(@Qualifier("c8SqlSessionFactory") SqlSessionFactory c8SqlSessionFactory) {
+    return createMapperFactoryBean(c8SqlSessionFactory, UsageMetricTUMapper.class);
+  }
+
+  @Bean
+  public VariableDbReader variableRdbmsReader(VariableMapper variableMapper) {
     return new VariableDbReader(variableMapper);
   }
 
   @Bean
-  public AuthorizationDbReader authorizationReader(final AuthorizationMapper authorizationMapper) {
+  public AuthorizationDbReader authorizationReader(AuthorizationMapper authorizationMapper) {
     return new AuthorizationDbReader(authorizationMapper);
   }
 
   @Bean
   public DecisionDefinitionDbReader decisionDefinitionReader(
-      final DecisionDefinitionMapper decisionDefinitionMapper) {
+      DecisionDefinitionMapper decisionDefinitionMapper) {
     return new DecisionDefinitionDbReader(decisionDefinitionMapper);
   }
 
   @Bean
   public DecisionInstanceDbReader decisionInstanceReader(
-      final DecisionInstanceMapper decisionInstanceMapper) {
+      DecisionInstanceMapper decisionInstanceMapper) {
     return new DecisionInstanceDbReader(decisionInstanceMapper);
   }
 
   @Bean
   public DecisionRequirementsDbReader decisionRequirementsReader(
-      final DecisionRequirementsMapper decisionRequirementsMapper) {
+      DecisionRequirementsMapper decisionRequirementsMapper) {
     return new DecisionRequirementsDbReader(decisionRequirementsMapper);
   }
 
   @Bean
   public FlowNodeInstanceDbReader flowNodeInstanceReader(
-      final FlowNodeInstanceMapper flowNodeInstanceMapper) {
+      FlowNodeInstanceMapper flowNodeInstanceMapper) {
     return new FlowNodeInstanceDbReader(flowNodeInstanceMapper);
   }
 
   @Bean
-  public GroupDbReader groupReader(final GroupMapper groupMapper) {
+  public GroupDbReader groupReader(GroupMapper groupMapper) {
     return new GroupDbReader(groupMapper);
   }
 
   @Bean
-  public IncidentDbReader incidentReader(final IncidentMapper incidentMapper) {
+  public IncidentDbReader incidentReader(IncidentMapper incidentMapper) {
     return new IncidentDbReader(incidentMapper);
   }
 
   @Bean
   public ProcessDefinitionDbReader processDeploymentRdbmsReader(
-      final ProcessDefinitionMapper processDefinitionMapper) {
+      ProcessDefinitionMapper processDefinitionMapper) {
     return new ProcessDefinitionDbReader(processDefinitionMapper);
   }
 
   @Bean
   public ProcessInstanceDbReader processRdbmsReader(
-      final ProcessInstanceMapper processInstanceMapper) {
+      ProcessInstanceMapper processInstanceMapper) {
     return new ProcessInstanceDbReader(processInstanceMapper);
   }
 
   @Bean
-  public TenantDbReader tenantReader(final TenantMapper tenantMapper) {
+  public TenantDbReader tenantReader(TenantMapper tenantMapper) {
     return new TenantDbReader(tenantMapper);
   }
 
   @Bean
-  public UserDbReader userRdbmsReader(final UserMapper userTaskMapper) {
+  public UserDbReader userRdbmsReader(UserMapper userTaskMapper) {
     return new UserDbReader(userTaskMapper);
   }
 
   @Bean
-  public RoleDbReader roleRdbmsReader(final RoleMapper roleMapper) {
+  public RoleDbReader roleRdbmsReader(RoleMapper roleMapper) {
     return new RoleDbReader(roleMapper);
   }
 
   @Bean
-  public UserTaskDbReader userTaskRdbmsReader(final UserTaskMapper userTaskMapper) {
+  public UserTaskDbReader userTaskRdbmsReader(UserTaskMapper userTaskMapper) {
     return new UserTaskDbReader(userTaskMapper);
   }
 
   @Bean
-  public FormDbReader formRdbmsReader(final FormMapper formMapper) {
+  public FormDbReader formRdbmsReader(FormMapper formMapper) {
     return new FormDbReader(formMapper);
   }
 
   @Bean
-  public MappingRuleDbReader mappingRdbmsReader(final MappingRuleMapper mappingMapper) {
+  public MappingRuleDbReader mappingRdbmsReader(MappingRuleMapper mappingMapper) {
     return new MappingRuleDbReader(mappingMapper);
   }
 
   @Bean
   public BatchOperationDbReader batchOperationReader(
-      final BatchOperationMapper batchOperationMapper) {
+      BatchOperationMapper batchOperationMapper) {
     return new BatchOperationDbReader(batchOperationMapper);
   }
 
   @Bean
   public BatchOperationItemDbReader batchOperationItemReader(
-      final BatchOperationMapper batchOperationMapper) {
+      BatchOperationMapper batchOperationMapper) {
     return new BatchOperationItemDbReader(batchOperationMapper);
   }
 
   @Bean
-  public JobDbReader jobReader(final JobMapper jobMapper) {
+  public JobDbReader jobReader(JobMapper jobMapper) {
     return new JobDbReader(jobMapper);
   }
 
@@ -330,6 +337,11 @@ public class C8Configuration extends AbstractConfiguration {
   @Bean
   public UsageMetricsDbReader usageMetricReader(UsageMetricMapper usageMetricMapper) {
     return new UsageMetricsDbReader(usageMetricMapper);
+  }
+
+  @Bean
+  public UsageMetricTUDbReader usageMetricTUReader(UsageMetricTUMapper usageMetricTUMapper) {
+    return new UsageMetricTUDbReader(usageMetricTUMapper);
   }
 
   @Bean
@@ -347,7 +359,8 @@ public class C8Configuration extends AbstractConfiguration {
       BatchOperationDbReader batchOperationReader,
       JobMapper jobMapper,
       SequenceFlowMapper sequenceFlowMapper,
-      UsageMetricMapper usageMetricMapper) {
+      UsageMetricMapper usageMetricMapper,
+      UsageMetricTUMapper usageMetricTUMapper) {
     return new RdbmsWriterFactory(
         c8SqlSessionFactory,
         exporterPositionMapper,
@@ -363,32 +376,35 @@ public class C8Configuration extends AbstractConfiguration {
         batchOperationReader,
         jobMapper,
         sequenceFlowMapper,
-        usageMetricMapper);
+        usageMetricMapper,
+        usageMetricTUMapper);
   }
 
   @Bean
   public RdbmsService rdbmsService(
-      final RdbmsWriterFactory rdbmsWriterFactory,
-      final VariableDbReader variableReader,
-      final AuthorizationDbReader authorizationReader,
-      final DecisionDefinitionDbReader decisionDefinitionReader,
-      final DecisionInstanceDbReader decisionInstanceReader,
-      final DecisionRequirementsDbReader decisionRequirementsReader,
-      final FlowNodeInstanceDbReader flowNodeInstanceReader,
-      final GroupDbReader groupReader,
-      final IncidentDbReader incidentReader,
-      final ProcessDefinitionDbReader processDefinitionReader,
-      final ProcessInstanceDbReader processInstanceReader,
-      final RoleDbReader roleReader,
-      final TenantDbReader tenantReader,
-      final UserDbReader userReader,
-      final UserTaskDbReader userTaskReader,
-      final FormDbReader formReader,
-      final BatchOperationDbReader batchOperationReader,
-      final SequenceFlowDbReader sequenceFlowReader,
-      final BatchOperationItemDbReader batchOperationItemReader,
-      final JobDbReader jobReader,
-      final UsageMetricsDbReader usageMetricsReader) {
+      RdbmsWriterFactory rdbmsWriterFactory,
+      VariableDbReader variableReader,
+      AuthorizationDbReader authorizationReader,
+      DecisionDefinitionDbReader decisionDefinitionReader,
+      DecisionInstanceDbReader decisionInstanceReader,
+      DecisionRequirementsDbReader decisionRequirementsReader,
+      FlowNodeInstanceDbReader flowNodeInstanceReader,
+      GroupDbReader groupReader,
+      IncidentDbReader incidentReader,
+      ProcessDefinitionDbReader processDefinitionReader,
+      ProcessInstanceDbReader processInstanceReader,
+      RoleDbReader roleReader,
+      TenantDbReader tenantReader,
+      UserDbReader userReader,
+      UserTaskDbReader userTaskReader,
+      FormDbReader formReader,
+      MappingRuleDbReader mappingRuleReader,
+      BatchOperationDbReader batchOperationReader,
+      SequenceFlowDbReader sequenceFlowReader,
+      BatchOperationItemDbReader batchOperationItemReader,
+      JobDbReader jobReader,
+      UsageMetricsDbReader usageMetricsReader,
+      UsageMetricTUDbReader usageMetricTUDbReader) {
     return new RdbmsService(
         rdbmsWriterFactory,
         authorizationReader,
@@ -406,12 +422,13 @@ public class C8Configuration extends AbstractConfiguration {
         userReader,
         userTaskReader,
         formReader,
-        null,
+        mappingRuleReader,
         batchOperationReader,
         sequenceFlowReader,
         batchOperationItemReader,
         jobReader,
-        usageMetricsReader);
+        usageMetricsReader,
+        usageMetricTUDbReader);
   }
 
 }
