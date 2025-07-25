d0fdf7520f20bdcf9b7b200eda0838f1e046735f
HeleneW-dot <helene.waechtler@camunda.com>
2025-07-25 07:59:40 +0200

    chore(idKeyMapper): clean up usage of mapper with client
    related to #5308

--- core/src/main/java/io/camunda/migrator/impl/logging/DbClientLogs.java
+++ core/src/main/java/io/camunda/migrator/impl/logging/DbClientLogs.java
@@ -24,12 +24,15 @@ public class DbClientLogs {
 
   // DbClient Error Messages
   public static final String FAILED_TO_CHECK_EXISTENCE = "Failed to check existence for legacyId: ";
+  public static final String FAILED_TO_FIND_ALL = "Failed to find all ids";
   public static final String FAILED_TO_FIND_LATEST_START_DATE = "Failed to find latest start date for type: ";
   public static final String FAILED_TO_FIND_LATEST_ID = "Failed to find latest legacyId for type: ";
   public static final String FAILED_TO_FIND_KEY_BY_ID = "Failed to find key by legacyId: ";
   public static final String FAILED_TO_UPDATE_KEY = "Failed to update key for legacyId: ";
   public static final String FAILED_TO_INSERT_RECORD = "Failed to insert record for legacyId: ";
   public static final String FAILED_TO_FIND_SKIPPED_COUNT = "Failed to find skipped count";
+  public static final String FAILED_TO_FIND_ALL_SKIPPED = "Failed to find skipped entity Ids";
+  public static final String FAILED_TO_DELETE = "Failed to delete mapping for legacyId: ";
 
   public static void updatingKeyForLegacyId(String legacyProcessInstanceId, Long processInstanceKey) {
     LOGGER.debug(UPDATING_KEY_FOR_LEGACY_ID, legacyProcessInstanceId, processInstanceKey);
