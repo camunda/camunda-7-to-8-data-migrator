f4a24d5613912c686cc4137349327b0a09eca46d
Joaquín <joaquin.felici@camunda.com>
2025-09-16 11:24:29 +0200

    squash: pr comments

--- core/src/main/java/io/camunda/migrator/config/mybatis/AbstractConfiguration.java
+++ core/src/main/java/io/camunda/migrator/config/mybatis/AbstractConfiguration.java
@@ -71,7 +71,7 @@ public class AbstractConfiguration {
     return factoryBean;
   }
 
-  public MultiTenantSpringLiquibase createSchema(DataSource dataSource, String tablePrefix, String changeLogFile) {
+  protected MultiTenantSpringLiquibase createSchema(DataSource dataSource, String tablePrefix, String changeLogFile) {
     String prefix = StringUtils.trimToEmpty(tablePrefix);
     ConfigurationLogs.logCreatingTableSchema(changeLogFile, prefix);
 
