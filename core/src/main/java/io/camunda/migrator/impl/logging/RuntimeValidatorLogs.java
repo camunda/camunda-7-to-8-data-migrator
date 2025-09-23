4908575e2fb2b0e3ee0ea68b7567d66e3feb699e
tasso94 <3015690+tasso94@users.noreply.github.com>
2025-09-23 15:37:05 +0200

    fix(runtime): fix validation message for call activity (#269)
    related to camunda/camunda-bpm-platform#5429

--- core/src/main/java/io/camunda/migrator/impl/logging/RuntimeValidatorLogs.java
+++ core/src/main/java/io/camunda/migrator/impl/logging/RuntimeValidatorLogs.java
@@ -32,7 +32,7 @@ public class RuntimeValidatorLogs {
   public static final String FLOW_NODE_NOT_EXISTS_ERROR = "Flow node with id [%s] doesn't exist in the equivalent deployed C8 model.";
   public static final String NO_C8_DEPLOYMENT_ERROR = "No C8 deployment found for process ID [%s] required for instance with C7 ID [%s].";
   public static final String FAILED_TO_PARSE_BPMN_MODEL = "Failed to parse BPMN model from XML";
-  public static final String CALL_ACTIVITY_LEGACY_ID_ERROR = "Found call activity with propagateAllParentVariables=false for flow node with id [%s] in C8 process. This is not supported by the migrator unless there is an explicit mapping for the C7 ID variable, as it would lead to orphaned sub-process instances.";
+  public static final String CALL_ACTIVITY_LEGACY_ID_ERROR = "Found call activity with propagateAllParentVariables=false for flow node with id [%s] in C8 process. This is not supported by the migrator unless there is an explicit mapping for the 'legacyId' variable, as it would lead to orphaned sub-process instances.";
   public static final String TENANT_ID_ERROR = "Found a process instance with tenant id [%s] that is not configured for migration.";
 
   public static void validateC7ProcessInstance(String c7ProcessInstanceId) {
