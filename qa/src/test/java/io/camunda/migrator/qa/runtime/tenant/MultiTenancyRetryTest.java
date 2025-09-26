d1ab3a89e0311d7b2c25d3a764774bf48551f8d3
Yana Vasileva <yana.vasileva@camunda.com>
2025-09-26 13:24:57 +0200

    impl review hints

--- qa/src/test/java/io/camunda/migrator/qa/runtime/tenant/MultiTenancyRetryTest.java
+++ qa/src/test/java/io/camunda/migrator/qa/runtime/tenant/MultiTenancyRetryTest.java
@@ -10,16 +10,20 @@ package io.camunda.migrator.qa.runtime.tenant;
 import static io.camunda.migrator.constants.MigratorConstants.C8_DEFAULT_TENANT;
 import static io.camunda.migrator.constants.MigratorConstants.LEGACY_ID_VAR_NAME;
 import static io.camunda.migrator.impl.logging.RuntimeMigratorLogs.SKIPPING_PROCESS_INSTANCE_VALIDATION_ERROR;
+import static io.camunda.migrator.impl.logging.RuntimeValidatorLogs.NO_C8_TENANT_DEPLOYMENT_ERROR;
 import static io.camunda.migrator.impl.logging.RuntimeValidatorLogs.TENANT_ID_ERROR;
 import static io.camunda.process.test.api.CamundaAssert.assertThat;
 import static io.camunda.process.test.api.assertions.ProcessInstanceSelectors.byKey;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.client.CamundaClient;
+import io.camunda.client.api.command.ClientStatusException;
 import io.camunda.migrator.MigratorMode;
 import io.camunda.migrator.RuntimeMigrator;
+import io.camunda.migrator.exception.RuntimeMigratorException;
 import io.camunda.migrator.qa.runtime.RuntimeMigrationAbstractTest;
 import io.github.netmikey.logunit.api.LogCapturer;
+import io.grpc.StatusRuntimeException;
 import org.camunda.bpm.engine.variable.Variables;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -47,33 +51,45 @@ class MultiTenancyRetryTest extends RuntimeMigrationAbstractTest {
   @BeforeEach
   void setupTenants() {
     // create tenant
-    client.newCreateTenantCommand().tenantId(TENANT_ID_1).name(TENANT_ID_1).send().join();
+    client.newCreateTenantCommand().tenantId(TENANT_ID_1).name(TENANT_ID_1).execute();
     // assign the default user to the tenant
-    client.newAssignUserToTenantCommand().username(DEFAULT_USERNAME).tenantId(TENANT_ID_1).send().join();
+    client.newAssignUserToTenantCommand().username(DEFAULT_USERNAME).tenantId(TENANT_ID_1).execute();
   }
 
   @Test
   public void shouldMigrateProcessInstanceWithTenantWhenAdded() {
     // given
     deployer.deployCamunda7Process(SIMPLE_PROCESS_BPMN, TENANT_ID_2);
-    deployer.deployCamunda8Process(SIMPLE_PROCESS_BPMN, TENANT_ID_1);
+    deployer.deployCamunda7Process(SIMPLE_PROCESS_BPMN, TENANT_ID_1);
 
-    String c7ProcessInstanceId = runtimeService.startProcessInstanceByKey(SIMPLE_PROCESS_ID).getId();
-
-    assertThatProcessInstanceCountIsEqualTo(0);
+    var definitionWithTenant1 = repositoryService.createProcessDefinitionQuery()
+        .tenantIdIn(TENANT_ID_1)
+        .singleResult()
+        .getId();
+    var definitionWithTenant2 = repositoryService.createProcessDefinitionQuery()
+        .tenantIdIn(TENANT_ID_2)
+        .singleResult()
+        .getId();
+    String c7WithT1 = runtimeService.startProcessInstanceById(definitionWithTenant1).getId();
+    String c7WithT2 = runtimeService.startProcessInstanceById(definitionWithTenant2).getId();
 
     try {
       runtimeMigrator.start();
-    } catch (Exception e) {
+    } catch (RuntimeMigratorException e) {
       // expected to throw an exception
+      assertThat(e.getCause().toString()).contains("user is not authorized");
     }
+
+    assertThatProcessInstanceCountIsEqualTo(0);
+
     // when
-    client.newCreateTenantCommand().tenantId(TENANT_ID_2).name(TENANT_ID_2).send().join();
-    client.newAssignUserToTenantCommand().username(DEFAULT_USERNAME).tenantId(TENANT_ID_2).send().join();
+    client.newCreateTenantCommand().tenantId(TENANT_ID_2).name(TENANT_ID_2).execute();
+    client.newAssignUserToTenantCommand().username(DEFAULT_USERNAME).tenantId(TENANT_ID_2).execute();
     deployer.deployCamunda8Process(SIMPLE_PROCESS_BPMN, TENANT_ID_2);
 
     runtimeMigrator.setMode(MigratorMode.RETRY_SKIPPED);
     runtimeMigrator.start();
+
     // then
     assertThatProcessInstanceCountIsEqualTo(1);
     var c8ProcessInstance = client.newProcessInstanceSearchRequest()
@@ -83,6 +99,8 @@ class MultiTenancyRetryTest extends RuntimeMigrationAbstractTest {
         .items()
         .getFirst();
     assertThat(c8ProcessInstance).isNotNull();
+    logs.assertContains(String.format(SKIPPING_PROCESS_INSTANCE_VALIDATION_ERROR.replace("{}", "%s"), c7WithT1,
+        String.format(NO_C8_TENANT_DEPLOYMENT_ERROR, SIMPLE_PROCESS_ID, TENANT_ID_1, c7WithT1)));
   }
 
 }
